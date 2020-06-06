b787c33eda0093f43ead6ed36483e568c2978749
Pedro Bravo <pedro.bravoa@yahoo.com>
2020-06-06 20:34:22 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
      array.join
 
   elsif array.length > 2 
-        array[-1].prepend "and " 
+        array[-1].prepend("and ")
         array.join(", ")
 
   elsif array.length == 2 
