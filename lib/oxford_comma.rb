4ef0b4b2ea4d54bf01bb1bf9c8caa8aee35c10a7
Pedro Bravo <pedro.bravoa@yahoo.com>
2020-06-09 04:46:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(frutas)
     frutas[-2] << " and " 
    frutas.join
   elsif frutas.length > 2 
-    frutas[-1].prepend("and ")
+    frutas[-1].unshift("and ")
    frutas.join(", ")
   end
 end
